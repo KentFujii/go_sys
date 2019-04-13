0ae5ea032e48fdb6b3e5c2654a99fd03a10f3a0f
KentFujii <kent.where.the.light.is@gmail.com>
2019-04-14 03:24:00 +0900

    finish: concurrency_pattern

--- concurrency_pattern/future_promise/main.go
+++ concurrency_pattern/future_promise/main.go
@@ -14,6 +14,7 @@ func readFile(path string) chan string {
 			fmt.Printf("read error %s\n", err.Error())
 			close(promise)
 		} else {
+			// 約束を果たした
 			promise <- string(content)
 		}
 	}()
@@ -24,11 +25,13 @@ func printFunc(futureSource chan string) chan []string {
 	promise := make(chan []string)
 	go func() {
 		var result []string
+		// futureSourceのFutureが解決するのをチャネルのブロッキングで待ってから実行
 		for _, line := range strings.Split(<-futureSource, "\n") {
 			if strings.HasPrefix(line, "func ") {
 				result = append(result, line)
 			}
 		}
+		// 約束を果たした
 		promise <- result
 	}()
 	return promise
